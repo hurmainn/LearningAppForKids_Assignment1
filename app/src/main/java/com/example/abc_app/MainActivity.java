ab4bbf330605073968d8bb9dd0175fdbc246a382
hurmainn <hurmainjavaid18@gmail.com>
2022-05-20 12:02:02 +0500

    Alphabets array created

--- app/src/main/java/com/example/abc_app/MainActivity.java
+++ app/src/main/java/com/example/abc_app/MainActivity.java
@@ -10,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Creating alphabets array
+        String[] alphabets={ };
+        alphabets = new String[26];
+        char ascii=65;
+        for(int i=0;i<26;i++)
+        {
+            alphabets[i]=Character.toString(ascii);         //fill the array with alphabets
+        }
+
+
     }
 }
