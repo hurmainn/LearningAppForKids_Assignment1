219379e6c1da18271bbc867627232c0da43d30db
hurmainn <hurmainjavaid18@gmail.com>
2022-05-20 15:09:28 +0500

    Second activity created, Buttons on click listeners set

--- app/src/main/java/com/example/abc_app/MainActivity.java
+++ app/src/main/java/com/example/abc_app/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.abc_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     GridView gridView;
     @Override
@@ -23,6 +25,75 @@ public class MainActivity extends AppCompatActivity {
             alphabets[i]=Character.toString(ascii);         //fill the array with alphabets
         }
 
+        //giving variables to buttons
+       Button A=findViewById(R.id.A);
+        Button B=findViewById(R.id.B);
+        Button C=findViewById(R.id.C);
+        Button D=findViewById(R.id.D);
+        Button E=findViewById(R.id.E);
+        Button F=findViewById(R.id.F);
+        Button G=findViewById(R.id.G);
+        Button H=findViewById(R.id.H);
+        Button I=findViewById(R.id.I);
+        Button J=findViewById(R.id.J);
+        Button K=findViewById(R.id.K);
+        Button L=findViewById(R.id.L);
+        Button M=findViewById(R.id.M);
+        Button N=findViewById(R.id.N);
+        Button O=findViewById(R.id.O);
+        Button P=findViewById(R.id.P);
+        Button Q=findViewById(R.id.Q);
+        Button R_=findViewById(R.id.S);
+        Button S=findViewById(R.id.S);
+        Button T=findViewById(R.id.T);
+        Button U=findViewById(R.id.U);
+        Button V=findViewById(R.id.V);
+        Button W=findViewById(R.id.W);
+        Button X=findViewById(R.id.X);
+        Button Y=findViewById(R.id.Y);
+        Button Z=findViewById(R.id.Z);
+
+        //setting on click listeners
+        A.setOnClickListener(this);
+        B.setOnClickListener(this);
+        C.setOnClickListener(this);
+        D.setOnClickListener(this);
+        E.setOnClickListener(this);
+        F.setOnClickListener(this);
+        G.setOnClickListener(this);
+        H.setOnClickListener(this);
+        I.setOnClickListener(this);
+        J.setOnClickListener(this);
+        K.setOnClickListener(this);
+        L.setOnClickListener(this);
+        M.setOnClickListener(this);
+        N.setOnClickListener(this);
+        O.setOnClickListener(this);
+        P.setOnClickListener(this);
+        Q.setOnClickListener(this);
+        R_.setOnClickListener(this);
+        S.setOnClickListener(this);
+        T.setOnClickListener(this);
+        U.setOnClickListener(this);
+        V.setOnClickListener(this);
+        W.setOnClickListener(this);
+        X.setOnClickListener(this);
+        Y.setOnClickListener(this);
+        Z.setOnClickListener(this);
+
+
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId())
+        {
+
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/abc_app/PicturePageActivity.java
@@ -0,0 +1,14 @@
+package com.example.abc_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PicturePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture_page);
+    }
+}
